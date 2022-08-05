02689871443f83882ce041b37863f8339f68f5f3
grantslape <grant@unit410.com>
2022-08-05 10:13:39 -0500

    add tests for private key leakage via public key variance

--- go.mod
+++ go.mod
@@ -6,5 +6,4 @@ require (
 	github.com/stretchr/testify v1.3.0
 	gitlab.com/unit410/edwards25519 v0.0.0-20220725154547-61980033348e
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
-	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 )
--- pkg/ed25519_test.go
+++ pkg/ed25519_test.go
@@ -89,55 +89,64 @@ func TestAddElements(test *testing.T) {
 	require.Equal(test, recombinedPublic, addedPublic)
 }
 
-func TestValid2Of3(test *testing.T) {
-	message := []byte("Hello World!")
+type keyPairWithShares struct {
+	publicKey  ed25519.PublicKey
+	privateKey ed25519.PrivateKey
+	shares     []Scalar
+}
 
+func generateKeyPairWithShares(t *testing.T) keyPairWithShares {
 	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
-	require.NoError(test, err)
+	require.NoError(t, err)
 
-	shares := DealShares(ExpandSecret(privateKey.Seed()), 2, 3)
+	return keyPairWithShares{
+		publicKey:  publicKey,
+		privateKey: privateKey,
+		shares:     DealShares(ExpandSecret(privateKey.Seed()), 2, 3),
+	}
+}
 
-	ephPublicKey, ephPrivateKey, err := ed25519.GenerateKey(rand.Reader)
-	require.NoError(test, err)
+func TestValid2Of3(t *testing.T) {
+	message := []byte("Hello World!")
+	keyPair := generateKeyPairWithShares(t)
+	ephKeyPair := generateKeyPairWithShares(t)
 
-	ephShares := DealShares(ExpandSecret(ephPrivateKey.Seed()), 2, 3)
-
-	shareSig1 := SignWithShare(message, shares[0], ephShares[0], publicKey, ephPublicKey)
-	shareSig2 := SignWithShare(message, shares[1], ephShares[1], publicKey, ephPublicKey)
-	shareSig3 := SignWithShare(message, shares[2], ephShares[2], publicKey, ephPublicKey)
+	shareSig1 := SignWithShare(message, keyPair.shares[0], ephKeyPair.shares[0], keyPair.publicKey, ephKeyPair.publicKey)
+	shareSig2 := SignWithShare(message, keyPair.shares[1], ephKeyPair.shares[1], keyPair.publicKey, ephKeyPair.publicKey)
+	shareSig3 := SignWithShare(message, keyPair.shares[2], ephKeyPair.shares[2], keyPair.publicKey, ephKeyPair.publicKey)
 
 	{
 		combinedSig := CombineShares(3, []int{1, 2, 3}, [][]byte{shareSig1, shareSig2, shareSig3})
-		signature := append(ephPublicKey, combinedSig...)
-		require.True(test, ed25519.Verify(publicKey, message, signature[:]), "Invalid Signature for signer [1,2,3]")
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		require.True(t, ed25519.Verify(keyPair.publicKey, message, signature[:]), "Invalid Signature for signer [1,2,3]")
 	}
 
 	{
 		combinedSig := CombineShares(3, []int{1, 2}, [][]byte{shareSig1, shareSig2})
-		signature := append(ephPublicKey, combinedSig...)
-		require.True(test, ed25519.Verify(publicKey, message, signature[:]), "Invalid Signature for signer [1,2]")
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		require.True(t, ed25519.Verify(keyPair.publicKey, message, signature[:]), "Invalid Signature for signer [1,2]")
 	}
 
 	{
 		combinedSig := CombineShares(3, []int{2, 3}, [][]byte{shareSig2, shareSig3})
-		signature := append(ephPublicKey, combinedSig...)
-		require.True(test, ed25519.Verify(publicKey, message, signature[:]), "Invalid Signature for signer [2,3]")
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		require.True(t, ed25519.Verify(keyPair.publicKey, message, signature[:]), "Invalid Signature for signer [2,3]")
 	}
 
 	{
 		combinedSig := CombineShares(3, []int{1, 3}, [][]byte{shareSig1, shareSig3})
-		signature := append(ephPublicKey, combinedSig...)
-		require.True(test, ed25519.Verify(publicKey, message, signature[:]), "Invalid Signature for signer [1,3]")
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		require.True(t, ed25519.Verify(keyPair.publicKey, message, signature[:]), "Invalid Signature for signer [1,3]")
 	}
 
 	{
 		combinedSig := CombineShares(3, []int{1}, [][]byte{shareSig1})
-		signature := append(ephPublicKey, combinedSig...)
-		require.False(test, ed25519.Verify(publicKey, message, signature[:]), "Signature should not be valid")
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		require.False(t, ed25519.Verify(keyPair.publicKey, message, signature[:]), "Signature should not be valid")
 	}
 }
 
-// manually forces the final secret in CombineShares to be < 32 bytes in length
+// TestCombine manually forces the final secret in CombineShares to be < 32 bytes in length
 // Tests that the reverse and copy into final signature happen in the correct order
 func TestCombine(test *testing.T) {
 	message := []byte("Hello World!")
@@ -148,68 +157,126 @@ func TestCombine(test *testing.T) {
 	shareSig1 := hexToBytes("75199ff37d42acc04cc5e2ed076e8dd3529382395b78f55ad0da80fbe331c809")
 	shareSig2 := hexToBytes("f79c539d0a0fe711cf12ebb67eee602893bfdf180910a9c1deada79ce1371c03")
 
+	combinedSig := CombineShares(3, []int{1, 2}, [][]byte{shareSig1, shareSig2})
+	signature := append(ephPublicKey, combinedSig...)
+	if !ed25519.Verify(publicKey, message, signature[:]) {
+		test.Error("Invalid Signature for signer [1,2]")
+	}
+}
+
+// TestInvalid1Of3 test that no single signer produces a valid signature
+func TestInvalid1Of3(t *testing.T) {
+	message := []byte("Hello World!")
+	keyPair := generateKeyPairWithShares(t)
+	ephKeyPair := generateKeyPairWithShares(t)
+
+	shareSig1 := SignWithShare(message, keyPair.shares[0], ephKeyPair.shares[0], keyPair.publicKey, ephKeyPair.publicKey)
+	shareSig2 := SignWithShare(message, keyPair.shares[1], ephKeyPair.shares[1], keyPair.publicKey, ephKeyPair.publicKey)
+	shareSig3 := SignWithShare(message, keyPair.shares[2], ephKeyPair.shares[2], keyPair.publicKey, ephKeyPair.publicKey)
+
 	{
-		combinedSig := CombineShares(3, []int{1, 2}, [][]byte{shareSig1, shareSig2})
-		signature := append(ephPublicKey, combinedSig...)
-		if !ed25519.Verify(publicKey, message, signature[:]) {
-			test.Error("Invalid Signature for signer [1,2]")
+		combinedSig := CombineShares(3, []int{1}, [][]byte{shareSig1})
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		if ed25519.Verify(keyPair.publicKey, message, signature[:]) {
+			t.Error("Incorrectly accepted signature")
+		}
+
+		{
+			signature := append(ephKeyPair.publicKey, shareSig1...)
+			if ed25519.Verify(keyPair.publicKey, message, signature[:]) {
+				t.Error("Incorrectly accepted signature")
+			}
+		}
+
+		{
+			signature := append(ephKeyPair.publicKey, shareSig1...)
+			if ed25519.Verify(keyPair.publicKey, message, signature[:]) {
+				t.Error("Incorrectly accepted signature")
+			}
+		}
+	}
+
+	{
+		combinedSig := CombineShares(3, []int{2}, [][]byte{shareSig2})
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		if ed25519.Verify(keyPair.publicKey, message, signature[:]) {
+			t.Error("Incorrectly accepted signature")
+		}
+	}
+
+	{
+		combinedSig := CombineShares(3, []int{3}, [][]byte{shareSig3})
+		signature := append(ephKeyPair.publicKey, combinedSig...)
+		if ed25519.Verify(keyPair.publicKey, message, signature[:]) {
+			t.Error("Incorrectly accepted signature")
 		}
 	}
 }
 
-// Test that no single signer produces a valid signature
-func TestInvalid1Of3(test *testing.T) {
+// TestKeyLeakage by signing a message with threshold shares,
+// then sign the same message with an invalid public key for the assembled key
+// the resulting leaked value should be the ephemeral public key which is public
+// this is more of a sanity test than anything else because presumably if you can substitute a malicious key, you
+// could just steal the private key shares in the first place. In the case of a developer mishap, only the
+// ephemeral public key is leaked which should not cause concern.
+func TestKeyLeakage(t *testing.T) {
+	signWithAllShares := func(
+		shares []Scalar,
+		ephShares []Scalar,
+		publicKey ed25519.PublicKey,
+		ephPublicKey ed25519.PublicKey,
+		message []byte,
+	) ed25519.PublicKey {
+		shareSig1 := SignWithShare(message, shares[0], ephShares[0], publicKey, ephPublicKey)
+		shareSig2 := SignWithShare(message, shares[1], ephShares[1], publicKey, ephPublicKey)
+		shareSig3 := SignWithShare(message, shares[2], ephShares[2], publicKey, ephPublicKey)
+		combinedSig := CombineShares(3, []int{1, 2, 3}, [][]byte{shareSig1, shareSig2, shareSig3})
+		return append(ephPublicKey, combinedSig...)
+	}
+
 	message := []byte("Hello World!")
+	keyPair := generateKeyPairWithShares(t)
+	ephKeyPair := generateKeyPairWithShares(t)
+	malKeyPair := generateKeyPairWithShares(t)
 
-	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
-	require.NoError(test, err)
+	sigValid := signWithAllShares(keyPair.shares, ephKeyPair.shares, keyPair.publicKey, ephKeyPair.publicKey, message)
+	// Sanity check our sig is valid
+	require.True(t, ed25519.Verify(keyPair.publicKey, message, sigValid[:]), "Invalid Signature for signer [1,2,3]")
 
-	shares := DealShares(privateKey.Seed(), 2, 3)
+	sigMal := signWithAllShares(keyPair.shares, ephKeyPair.shares, malKeyPair.publicKey, ephKeyPair.publicKey, message)
+	// Sanity check that malicious sig is invalid
+	require.False(t, ed25519.Verify(keyPair.publicKey, message, sigMal[:]), "Valid Signature for malicious signer [1,2,3]")
+	require.False(t, ed25519.Verify(malKeyPair.publicKey, message, sigMal[:]), "Valid Signature for malicious signer [1,2,3]")
 
-	ephPublicKey, ephPrivateKey, err := ed25519.GenerateKey(rand.Reader)
-	require.NoError(test, err)
-
-	ephShares := DealShares(ephPrivateKey.Seed(), 2, 3)
-
-	shareSig0 := SignWithShare(message, shares[0], ephShares[0], publicKey, ephPublicKey)
-	shareSig1 := SignWithShare(message, shares[1], ephShares[1], publicKey, ephPublicKey)
-	shareSig2 := SignWithShare(message, shares[2], ephShares[2], publicKey, ephPublicKey)
-
-	{
-		combinedSig := CombineShares(3, []int{1}, [][]byte{shareSig0})
-		signature := append(ephPublicKey, combinedSig...)
-		if ed25519.Verify(publicKey, message, signature[:]) {
-			test.Error("Incorrectly accepted signature")
-		}
-
-		{
-			signature := append(ephPublicKey, shareSig0...)
-			if ed25519.Verify(publicKey, message, signature[:]) {
-				test.Error("Incorrectly accepted signature")
-			}
-		}
-
-		{
-			signature := append(ephPublicKey, shareSig0...)
-			if ed25519.Verify(publicKey, message, signature[:]) {
-				test.Error("Incorrectly accepted signature")
-			}
-		}
+	if hex.EncodeToString(sigValid[:32]) == hex.EncodeToString(sigMal[:32]) {
+		require.Equal(t, hex.EncodeToString(ephKeyPair.publicKey), hex.EncodeToString(sigValid[:32]), "Leaked R value was not ephemeral public key")
 	}
+}
 
-	{
-		combinedSig := CombineShares(3, []int{2}, [][]byte{shareSig1})
-		signature := append(ephPublicKey, combinedSig...)
-		if ed25519.Verify(publicKey, message, signature[:]) {
-			test.Error("Incorrectly accepted signature")
-		}
-	}
+// TestShareLeakageByEphemeralKey ensures that R signature values are not leaked by varying the ephemeral public key
+// used in threshold signing.
+func TestShareLeakageByEphemeralKey(t *testing.T) {
+	message := []byte("Hello World!")
+	keyPair := generateKeyPairWithShares(t)
+	ephKeyPair := generateKeyPairWithShares(t)
+	ephKeyPairMal := generateKeyPairWithShares(t)
 
-	{
-		combinedSig := CombineShares(3, []int{3}, [][]byte{shareSig2})
-		signature := append(ephPublicKey, combinedSig...)
-		if ed25519.Verify(publicKey, message, signature[:]) {
-			test.Error("Incorrectly accepted signature")
-		}
-	}
+	// can we vary ephPublicKey and get r?
+	shareSig0 := SignWithShare(message, keyPair.shares[0], ephKeyPair.shares[0], keyPair.publicKey, ephKeyPair.publicKey)
+	shareSig0Mal := SignWithShare(message, keyPair.shares[0], ephKeyPair.shares[0], keyPair.publicKey, ephKeyPairMal.publicKey)
+	require.NotEqual(t, hex.EncodeToString(shareSig0), hex.EncodeToString(shareSig0Mal))
+}
+
+// TestShareLeakageByEphemeralKeyShare ensures that R signature values are not leaked by varying the
+// ephemeral public key share used in threshold signing
+func TestShareLeakageByEphemeralKeyShare(t *testing.T) {
+	message := []byte("Hello World!")
+	keyPair := generateKeyPairWithShares(t)
+	ephKeyPair := generateKeyPairWithShares(t)
+	ephKeyPairMal := generateKeyPairWithShares(t)
+
+	// Can we vary ephShares and get r?
+	shareSig0 := SignWithShare(message, keyPair.shares[0], ephKeyPair.shares[0], keyPair.publicKey, ephKeyPair.publicKey)
+	shareSig0Mal := SignWithShare(message, keyPair.shares[0], ephKeyPairMal.shares[0], keyPair.publicKey, ephKeyPairMal.publicKey)
+	require.NotEqual(t, hex.EncodeToString(shareSig0), hex.EncodeToString(shareSig0Mal))
 }
